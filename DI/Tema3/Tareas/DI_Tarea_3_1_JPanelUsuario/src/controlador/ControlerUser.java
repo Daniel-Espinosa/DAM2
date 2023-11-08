37d2ee74a92f82a00d092bba872bba7c2938caec
Daniel-Espinosa <despinosag02@educantabria.es>
2023-11-08 17:45:27 +0100

    DI / Tema 3 / Tarea 1

---
+++ DI/Tema3/Tareas/DI_Tarea_3_1_JPanelUsuario/src/controlador/ControlerUser.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import di_tarea_3_1_jpanelusuario.Main.Main;
+import entidad.User;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author damt207
+ */
+public class ControlerUser implements Serializable {
+
+    //DireccionDelFicheroUsuarios
+    public static ArrayList<User> arrayUser = new ArrayList<>();
+    private final static File ARCHIVO = new File("Usuarios.dat");
+
+    public static void leer_coleccion_del_fichero() {
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(ARCHIVO)) ){
+            // comprueba que el archivo existe y si es asi carga la informacion en el array list.
+            if (ARCHIVO.isFile()) {
+                
+                arrayUser = (ArrayList) ois.readObject();
+                //System.out.println("archivo " + ARCHIVO.getName() + " Cargado");
+            } else {
+                //System.out.println("El archivo " + ARCHIVO.getName() + " no existe, No se carga nada");
+            }
+
+        } catch (FileNotFoundException e) {
+            System.err.println("FileNotFoundException");
+        } catch (IOException e) {
+            System.err.println("IOException");
+        } catch (ClassNotFoundException ex) {
+            System.err.println("ClassNotFoundException");
+        
+        }
+    }
+
+    public static void grabar_coleccion_en_fichero() {
+
+
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(ARCHIVO))) {
+
+            oos.writeObject(arrayUser);
+
+        } catch (FileNotFoundException e) {
+            System.err.println("FileNotFoundException");
+        } catch (IOException e) {
+            System.err.println("IOException");
+        }
+    }
+
+    public static boolean conectar(String user, String pass) {
+        // si no hay Usuarios en el array devuelve un mensaje de que esta vacio
+        if (arrayUser.isEmpty()) {
+            System.err.println("La lista esta vacia, no se puede buscar un Usuario");
+            return false;
+            
+        } else {
+            //se recorre el array buscando coincidencia con el nombre Ignorando Case
+            for (User i : arrayUser) {
+                if (user.equalsIgnoreCase(i.getUser()) && pass.equals(i.getPass())) {
+                    Main.usuarioLogin.setName(i.getName());
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public static boolean userExist (String user){
+        
+        if (arrayUser.isEmpty()) {
+            System.err.println("La lista esta vacia, no se puede buscar un Usuario");
+            return false;
+        } else {
+            //se recorre el array buscando coincidencia con el nombre Ignorando Case
+            for (User i : arrayUser) {
+                if (user.equalsIgnoreCase(i.getUser())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    
+    public static void addUsuario(String user, String pass, String nombre, String rutaImagen){
+        
+        User u = new User(user, pass, nombre, rutaImagen);
+        arrayUser.add(u);
+        
+    }
+    
+
+}
---
+++ DI/Tema3/Tareas/DI_Tarea_3_1_JPanelUsuario/src/entidad/User.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidad;
+
+import java.io.File;
+import java.io.Serializable;
+
+/**
+ *
+ * @author damt207
+ */
+public class User implements Serializable {
+
+    private String user;
+    private String pass;
+    private String name;
+    private String rutaImagen;
+
+    public User(String user, String pass, String name, String rutaImagen) {
+        this.user = user;
+        this.pass = pass;
+        this.name = name;
+        this.rutaImagen = rutaImagen;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRutaImagen() {
+        return rutaImagen;
+    }
+
+    public void setRutaImagen(String rutaImagen) {
+        this.rutaImagen = rutaImagen;
+    }
+
+}
