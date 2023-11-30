c76128698d1c35ebca2c5452929cbe5868826ff9
Daniel-Espinosa <anareldis@gmail.com>
2023-11-30 22:58:48 +0100

    DI / Tema 2 / Pruebas

--- DI/Tema2/Ejercicios/PruebasFX/src/main/java/Controller/LoginController.java
+++ DI/Tema2/Ejercicios/PruebasFX/src/main/java/Controller/LoginController.java
@@ -4,16 +4,20 @@
  */
 package Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
@@ -36,6 +40,8 @@ public class LoginController implements Initializable {
     private TextField userTextField;
     @FXML
     private Button helloWorldButton;
+    @FXML
+    private VBox vboxGames;
 
     /**
      * Initializes the controller class.
@@ -71,10 +77,28 @@ public class LoginController implements Initializable {
     private void entroElRaton(MouseEvent event) {
         System.out.println("entro el raton");
     }
-
+    
+    private int cont = 0;
+    
     @FXML
     private void clickHelloWorldButton(ActionEvent event) {
-        userTextField.setText("Hello World");
+        try {
+            cont++; // Contador para que se vea que cambia!
+            
+            Pane pane = new Pane(App.loadFXML("AddGame")); //llamo a el pañel generico que solo tiene un puto LABEL!
+            
+            vboxGames.getChildren().add(pane); //añado el panel generico a una VBOX por poner algo a lo que añadir
+            
+            Label lb = (Label) pane.lookup("#titulo"); //UN PUTO METODO QUE BUSCA ALGO POR EL ID!!!!!!!!!!!!!!!!!!!!!!!
+            System.out.println(lb);
+            
+            lb.setText("Titulo: " + cont); //seteo al Label con lo que quiero!!!!!!
+            
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+        
     }
     
 }
