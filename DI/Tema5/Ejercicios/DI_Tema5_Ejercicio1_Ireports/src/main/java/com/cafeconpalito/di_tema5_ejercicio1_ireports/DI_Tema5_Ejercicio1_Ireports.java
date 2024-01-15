3aaedd40cd8a166b79391d8c25c03e1c45b5efb5
Daniel-Espinosa <121551604+Daniel-Espinosa@users.noreply.github.com>
2024-01-15 19:29:45 +0100

    DI / Tema 5 / Ejercicio 1
    Sin terminar

---
+++ DI/Tema5/Ejercicios/DI_Tema5_Ejercicio1_Ireports/src/main/java/com/cafeconpalito/di_tema5_ejercicio1_ireports/DI_Tema5_Ejercicio1_Ireports.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.cafeconpalito.di_tema5_ejercicio1_ireports;
+
+/**
+ *
+ * @author damt207
+ */
+public class DI_Tema5_Ejercicio1_Ireports {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
