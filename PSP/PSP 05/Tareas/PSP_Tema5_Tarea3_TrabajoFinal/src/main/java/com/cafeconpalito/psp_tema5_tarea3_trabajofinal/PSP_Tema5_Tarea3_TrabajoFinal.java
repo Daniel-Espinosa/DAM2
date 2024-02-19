b2d6890ca23114adfb6cb635c8a2ac0d95e960ee
Daniel-Espinosa <121551604+Daniel-Espinosa@users.noreply.github.com>
2024-02-19 21:02:22 +0100

    PSP / Tema 5 / Tarea 3

--- PSP/PSP 05/Tareas/PSP_Tema5_Tarea3_TrabajoFinal/src/main/java/com/cafeconpalito/psp_tema5_tarea3_trabajofinal/PSP_Tema5_Tarea3_TrabajoFinal.java
+++ PSP/PSP 05/Tareas/PSP_Tema5_Tarea3_TrabajoFinal/src/main/java/com/cafeconpalito/psp_tema5_tarea3_trabajofinal/PSP_Tema5_Tarea3_TrabajoFinal.java
@@ -3,11 +3,9 @@
  */
 package com.cafeconpalito.psp_tema5_tarea3_trabajofinal;
 
-import java.io.IOException;
-import java.util.logging.FileHandler;
+import com.cafeconpalito.psp_tema5_tarea3_trabajofinal.Metodos.HashTool;
+import com.cafeconpalito.psp_tema5_tarea3_trabajofinal.Metodos.Logs;
 import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 /**
  *
@@ -15,45 +13,18 @@ import java.util.logging.SimpleFormatter;
  */
 public class PSP_Tema5_Tarea3_TrabajoFinal {
 
-    public static final Logger LOGGER_ERRORS = Logger.getLogger("LoggerErros");
-    public static final Logger LOGGER_USER = Logger.getLogger("LoggerUser");
-
+    
     public static void main(String[] args) {
-        FileHandler fileHandlerErrors = null;
-        FileHandler fileHandlerUser = null;
-
-        try {
-
-            SimpleFormatter formatter = new SimpleFormatter();
-
-            //Log ERRORES
-            fileHandlerErrors = new FileHandler("logsErrors.txt", true);
-            LOGGER_ERRORS.addHandler(fileHandlerErrors);
-            fileHandlerErrors.setFormatter(formatter);
-
-            //Añado info al log
-            LOGGER_ERRORS.warning("Este es un mensaje de advertencia.");
-            LOGGER_ERRORS.severe("Este es un mensaje de error.");
-
-            //Log USUARIO
-            fileHandlerUser = new FileHandler("logsUser.txt", true);
-            LOGGER_USER.addHandler(fileHandlerUser);
-            fileHandlerUser.setFormatter(formatter);
-
-            //Añado la info al log
-            LOGGER_USER.info("Este es un mensaje de informacion.");
-
-
-        } catch (IOException ex) {
-            Logger.getLogger(PSP_Tema5_Tarea3_TrabajoFinal.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SecurityException ex) {
-            Logger.getLogger(PSP_Tema5_Tarea3_TrabajoFinal.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-
-            fileHandlerErrors.close();
-            fileHandlerUser.close();
-
-        }
+        
+        Logs.openLogs();
+        
+        //HashTool.getAlgorithms();
+        
+        HashTool.getStringHash("MD5", "ERA UN DOMINGO EN LA TARDE EN LOS COCHES DE CHOQUE PRIPAIRIAPRA");
+        
+        Logs.closeLogs();
 
     }
+    
+
 }
